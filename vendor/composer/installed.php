f82720d09dfc61cfc5532aba39bbb5001cef6fb7
dgrammatiko <d.grammatiko@gmail.com>
2021-12-24 13:11:14 +0100

    up

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'e11feb15b28de680d34b12a17877735905bbe6d3',
+    'name' => 'ttc/intervention-image',
+  ),
+  'versions' => 
+  array (
+    'ttc/intervention-image' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e11feb15b28de680d34b12a17877735905bbe6d3',
+    ),
+  ),
+);
